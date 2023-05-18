a62a63058d4364b8b85d79d7989d6af451e399c0
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2023-05-18 11:04:48 -0300

    Praticando pilhas (LIFO)

---
+++ baralhoDeCartas/baralhoDeCartas.js
@@ -0,0 +1,36 @@
+/*
+    Programa em JS que simula um baralho de cartas. O programa inicia mostrando na tela um menu interativo contendo as cartas que estão atualmente no baralho e as opções de "Adicionar uma carta", "Puxar uma carta" e "Sair". Ao escolher "Adicionar uma carta", o programa pergunta o nome da carta e a adiciona no topo do baralho. Ao escolher "Puxar uma carta", o programa retira a carta do topo do baralho e mostra na tela o nome da carta puxada. O programa só é encerrado ao escolher a opção "Sair", caso contrário volta ao menu.
+*/
+
+let baralho = []
+let opcao = ""
+
+do {
+    let cartas = ""
+
+    for (let i = 0; i < baralho.length; i++) {
+        cartas += '- ' + baralho[i] + '\n'
+    }
+
+    opcao = prompt(`Selecione a opção desejada: \na) Adicionar uma carta \nb) Puxar uma carta \nc) Sair \n\nAs cartas que estão atualmente no baralho são: \n${cartas}\n`)
+
+    switch(opcao) {
+        case 'a':
+            let cartaAdicionada = prompt('Informe o nome da carta')
+            baralho.push(cartaAdicionada)
+            break
+        
+        case 'b':
+            let cartaRemovida = baralho.pop(cartas)
+            alert(`A carta removida foi ${cartaRemovida}`)
+            break
+
+        case 'c':
+            alert('Encerrando sistema.')
+            break
+            
+        default:
+            alert('Opção inválida.')
+    }
+
+} while (opcao !== 'c')

00ce923b80081d997c57e9d1ebe507d100b74c83
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2023-05-25 16:59:54 -0300

    formularios

---
+++ DOM/ex008-formularios/index.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('orderForm')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault() //previne o evento padrão dos formulários de atualizar a página
+
+    const name = document.querySelector('input[name="name"]').value
+    const address = document.querySelector('input[name="address"]').value
+    const breadType = document.querySelector('select[name="breadType"]').value
+    const main = document.querySelector('input[name="main"]').value
+    const message = document.querySelector('textarea[name="message"]').value
+
+    let salad = ''
+    document.querySelectorAll('input[name="salad"]:checked').forEach(function (item) {
+        salad += ' - ' + item.value + '\n'
+    })
+
+    console.log({
+        name,
+        address,
+        breadType,
+        main,
+        salad,
+        message
+    })
+
+    alert(
+        "Pedido Realizado!" +
+        "\nNome do cliente: " + name +
+        "\nEndereço de entrega: " + address +
+        "\nTipo de pão: " + breadType +
+        "\nRecheio: \n - " + main + "\n" + salad +
+        "Observações: " + message
+      )
+})

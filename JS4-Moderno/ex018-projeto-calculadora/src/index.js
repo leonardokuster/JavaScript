98995a8a6b38c9859f246c7c5a22cb4cda4d7cef
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2023-08-17 11:23:21 -0300

    babel e webpack

---
+++ JS4-Moderno/ex018-projeto-calculadora/src/index.js
@@ -0,0 +1,76 @@
+import './styles/index.css'
+
+const body = document.querySelector('body')
+const main = document.querySelector('main')
+const header = document.querySelector('header')
+const root = document.querySelector(':root')
+const input = document.getElementById('input')
+const resultInput = document.getElementById('result')
+const allowedKeys = ['(', ')', '/', '*', '+', '-', '%', '.', ' ', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0']
+
+document.querySelectorAll('.charkey').forEach(function (charKeyBtn) {
+    charKeyBtn.addEventListener('click', function() {
+        const value = charKeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function() {
+    input.value = ''
+    input.focus()
+})
+
+input.addEventListener('keydown', function (ev) {
+    ev.preventDefault()
+
+    if(allowedKeys.includes(ev.key)){
+        input.value += ev.key
+        return
+    }
+    if(ev.key === 'Backspace'){
+        input.value = input.value.slice(0, -1)
+    }
+    if(ev.key === 'Enter'){
+        calculate()
+    }
+})
+
+document.getElementById('equal').addEventListener('click', calculate)
+
+
+function calculate() {
+    resultInput.value = "ERROR"
+    resultInput.classList.add("error")
+    const result = eval(input.value)
+    resultInput.value = result
+    resultInput.classList.remove("error")
+}
+
+document.getElementById('copyResult'). addEventListener('click', function(ev) {
+    const button = ev.currentTarget
+    if (button.innerText === 'Copy') {
+        button.innerText = 'Coppied!'
+        button.classList.add('success')
+        navigator.clipboard.writeText(resultInput.value)
+    } else {
+        button.innerText = 'Copy'
+        button.classList.remove('success')
+    }
+})
+
+  
+document.getElementById('switchBtn').addEventListener('click', function() {
+    if (body.dataset.theme === 'dark') {
+        root.style.setProperty("--bg-color", "#f1f5f9")
+        root.style.setProperty("--border-color", "#aaa")
+        root.style.setProperty("--font-color", "#212529")
+        root.style.setProperty("--primary-color", "#26834a")
+        body.dataset.theme = 'light'
+    } else {
+        root.style.setProperty("--bg-color", "#212529")
+        root.style.setProperty("--border-color", "#666")
+        root.style.setProperty("--font-color", "#f1f5f9")
+        root.style.setProperty("--primary-color", "#4dff91")
+        body.dataset.theme = 'dark'
+    }
+})
